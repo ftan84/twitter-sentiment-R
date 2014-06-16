ed516675d4fddc00554d4a2c2b31728eee90c1ec
ftan84 <francisatan@gmail.com>
2014-06-16 16:29:10 -0700

    Added lexicon

--- twitter-sentiment-R.R
+++ twitter-sentiment-R.R
@@ -4,11 +4,17 @@ library(plyr)
 library(stringr)
 library(ggplot2)
 
-query <- 'slothweek') # The word we want to analyze. Change this
+query <- 'slothweek' # The word we want to analyze. Change this
 maxTweets <- 10000 # The maximum number of tweets to search
+startDate <- '2014-01-01'
 
 consumer.key <- 'v5b3mm9hRjZ7kL7gcOUvxwr4m'
 consumer.secret <- 't2NZr2T18jOEFcX8h2B1jS8k8JH5jwD2eVUZBFyGAdEVI22GT6'
 access.token <- '1149944478-vXoBfwZ718WW12m6xFjoGHEvjQk73H1qP1g2q0Z'
 access.token.secret <- 'muJVicR46pW7dlMqxDkyAakJMDtijFyPeMCzcETU5ohf0'
 setup_twitter_oauth(consumer_key=consumer.key, consumer_secret=consumer.secret, access_token=access.token, access_secret=access.token.secret)
+
+# Retrieve tweets based on query
+tweets <- searchTwitter(query, n=maxTweets, since=startDate)
+tweets.df <- twListToDF(tweets)
+write.csv(tweets.df, file='data.csv', row.names=FALSE)
