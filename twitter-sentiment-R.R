bb9b10966592686f9bdb2ca42c139c7cff081fd6
Francis Tan <francisatan@gmail.com>
2016-02-26 13:21:01 -0800

    Update twitter-sentiment-R.R

--- twitter-sentiment-R.R
+++ twitter-sentiment-R.R
@@ -19,10 +19,11 @@ query <- 'slothweek' # The word we want to analyze. Change this
 maxTweets <- 10000 # The maximum number of tweets to search
 startDate <- '2014-01-01'
 
-consumer.key <- 'v5b3mm9hRjZ7kL7gcOUvxwr4m'
-consumer.secret <- 't2NZr2T18jOEFcX8h2B1jS8k8JH5jwD2eVUZBFyGAdEVI22GT6'
-access.token <- '1149944478-vXoBfwZ718WW12m6xFjoGHEvjQk73H1qP1g2q0Z'
-access.token.secret <- 'muJVicR46pW7dlMqxDkyAakJMDtijFyPeMCzcETU5ohf0'
+# Add your relevant keys here
+consumer.key <- [CONSUMER KEY]
+consumer.secret <- [CONSUMER SECRET]
+access.token <- [ACCESS TOKEN]
+access.token.secret <- [ACCESS TOKEN SECRET]
 setup_twitter_oauth(consumer_key=consumer.key, consumer_secret=consumer.secret, access_token=access.token, access_secret=access.token.secret)
 
 score.sentiment <- function(sentences, pos.words, neg.words, .progress='none') {
